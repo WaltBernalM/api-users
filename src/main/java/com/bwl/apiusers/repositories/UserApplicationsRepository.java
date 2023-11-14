0b7b3ecabfa311de92a7ab500701325f62d1ca15
Walter Bernal <wltrbm@gmail.com>
2023-11-14 16:42:32 -0700

    UserApplicaiton Repository Creation

---
+++ src/main/java/com/bwl/apiusers/repositories/UserApplicationsRepository.java
@@ -0,0 +1,6 @@
+package com.bwl.apiusers.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserApplicationsRepository extends JpaRepository<UserApplicationsRepository, Integer> {
+}
