d811ea977b364664c8a7031167b87326f23d4c84
Walter Bernal <wltrbm@gmail.com>
2023-11-14 16:54:02 -0700

    Creation of Profile model

---
+++ src/main/java/com/bwl/apiusers/models/Profile.java
@@ -0,0 +1,27 @@
+package com.bwl.apiusers.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "PROFILES")
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "KEYCODE", nullable = false)
+    private String keycode;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_APPLICATION", nullable = false)
+    private Integer idApplication;
+}
