2a24bc9fb48d1b03ecaf78bbccc586ee1ce10f40
Walter Bernal <wltrbm@gmail.com>
2023-11-15 13:32:04 -0700

    Creation of Factory Controller

---
+++ src/main/java/com/bwl/apiusers/controllers/FactoryController.java
@@ -0,0 +1,14 @@
+package com.bwl.apiusers.controllers;
+
+import com.bwl.apiusers.exceptions.PermissionNotFoundException;
+import com.bwl.apiusers.models.Application;
+import com.bwl.apiusers.models.Permission;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public abstract class FactoryController {
+
+}
