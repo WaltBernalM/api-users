b6e34ea7034fc47a3faeccfa7c04556692b7fe7c
Walter Bernal <wltrbm@gmail.com>
2023-11-15 11:52:40 -0700

    addition of methods to findByNameContaining

--- src/main/java/com/bwl/apiusers/repositories/ApplicationRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/ApplicationRepository.java
@@ -1,9 +1,16 @@
 package com.bwl.apiusers.repositories;
 
 import com.bwl.apiusers.models.Application;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
+    Page<Application> findByNameContaining(String name, Pageable pageable);
+    List<Application> findByNameContaining(String name, Sort sort);
 }
