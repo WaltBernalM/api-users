70839e591aa6e105396b62033e3c720c0f400cd1
Walter Bernal <wltrbm@gmail.com>
2023-11-14 17:23:48 -0700

    ApplicationModelAssembler created

---
+++ src/main/java/com/bwl/apiusers/assemblers/ApplicationModelAssembler.java
@@ -0,0 +1,19 @@
+package com.bwl.apiusers.assemblers;
+
+import com.bwl.apiusers.controllers.ApplicationController;
+import com.bwl.apiusers.models.Application;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class ApplicationModelAssembler implements RepresentationModelAssembler<Application, EntityModel<Application>> {
+    @Override
+    public EntityModel<Application> toModel(Application application) {
+        return EntityModel.of(application, //
+                linkTo(methodOn(ApplicationController.class).one(application.getId())).withSelfRel(),
+                linkTo(methodOn(ApplicationController.class).all()).withRel("applications"));
+    }
+}
