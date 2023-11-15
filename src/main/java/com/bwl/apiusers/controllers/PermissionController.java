fda66651ca66e0cd7ed0ca28c6ab001620da5291
Walter Bernal <wltrbm@gmail.com>
2023-11-15 12:27:49 -0700

    Endpoint to get one permission

--- src/main/java/com/bwl/apiusers/controllers/PermissionController.java
+++ src/main/java/com/bwl/apiusers/controllers/PermissionController.java
@@ -1,8 +1,12 @@
 package com.bwl.apiusers.controllers;
 
 import com.bwl.apiusers.assemblers.PermissionModelAssembler;
+import com.bwl.apiusers.exceptions.PermissionNotFoundException;
+import com.bwl.apiusers.models.Permission;
 import com.bwl.apiusers.repositories.PermissionRepository;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +21,9 @@ public class PermissionController {
         this.assembler = assembler;
     }
 
-//    @GetMapping("{id}")
-
+    @GetMapping("{id}")
+    public EntityModel<Permission> one (@PathVariable Integer id) {
+        Permission permission = repository.findById(id).orElseThrow(() -> new PermissionNotFoundException(id));
+        return assembler.toModel(permission);
+    }
 }
