5c245dfd74bf1f31fbe20e305137094f6255e004
Walter Bernal <wltrbm@gmail.com>
2023-11-15 12:08:45 -0700

    Creation of permission model assembler

---
+++ src/main/java/com/bwl/apiusers/assemblers/PermissionModelAssembler.java
@@ -0,0 +1,14 @@
+package com.bwl.apiusers.assemblers;
+
+import com.bwl.apiusers.models.Application;
+import com.bwl.apiusers.models.Permission;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+public class PermissionModelAssembler implements RepresentationModelAssembler<Permission, EntityModel<Permission>>{
+    @Override
+    public EntityModel<Permission> toModel(Permission permission) {
+        return EntityModel.of(permission);
+    }
+}
