50295e3f350c7b208b52f67e160af46fb54155cc
Walter Bernal <wltrbm@gmail.com>
2023-11-15 12:18:41 -0700

    Annotation add

--- src/main/java/com/bwl/apiusers/assemblers/PermissionModelAssembler.java
+++ src/main/java/com/bwl/apiusers/assemblers/PermissionModelAssembler.java
@@ -1,11 +1,11 @@
 package com.bwl.apiusers.assemblers;
 
-import com.bwl.apiusers.models.Application;
 import com.bwl.apiusers.models.Permission;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+@Component
 public class PermissionModelAssembler implements RepresentationModelAssembler<Permission, EntityModel<Permission>>{
     @Override
     public EntityModel<Permission> toModel(Permission permission) {
