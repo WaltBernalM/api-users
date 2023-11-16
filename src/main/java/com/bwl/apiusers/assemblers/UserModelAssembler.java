5d6131536c03ed0a33c9044f9413db79ee9691fe
Walter Bernal <wltrbm@gmail.com>
2023-11-16 14:43:50 -0700

    extension of UserModel assembler to DTOModel Assmebler

--- src/main/java/com/bwl/apiusers/assemblers/UserModelAssembler.java
+++ src/main/java/com/bwl/apiusers/assemblers/UserModelAssembler.java
@@ -1,8 +1,11 @@
 package com.bwl.apiusers.assemblers;
 
+import com.bwl.apiusers.dtos.UserDTO;
 import com.bwl.apiusers.models.User;
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserModelAssembler extends BaseModelAssembler<User>{
+@Data
+public class UserModelAssembler extends DTOModelAssembler<UserDTO, User> {
 }
