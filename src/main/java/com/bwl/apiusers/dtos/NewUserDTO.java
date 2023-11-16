fb2cb935798385b2b188be46dbd5b1d08281a971
Walter Bernal <wltrbm@gmail.com>
2023-11-16 12:58:56 -0700

    DTO model for new user input

--- src/main/java/com/bwl/apiusers/dtos/NewUserDTO.java
+++ src/main/java/com/bwl/apiusers/dtos/NewUserDTO.java
@@ -1,7 +1,10 @@
 package com.bwl.apiusers.dtos;
 
+import jakarta.persistence.Entity;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
+@Component
 @Data
 public class NewUserDTO implements DTO {
     private String email;
@@ -10,4 +13,6 @@ public class NewUserDTO implements DTO {
     private Integer profileId;
     private Integer applicationId;
     private String password;
+
+    public NewUserDTO(){}
 }
