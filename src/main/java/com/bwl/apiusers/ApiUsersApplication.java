1f87ad015b466a3c4f1af271ae9878ffa72f7a3d
Walter Bernal <wltrbm@gmail.com>
2023-11-16 12:56:14 -0700

    exclude security default login

--- src/main/java/com/bwl/apiusers/ApiUsersApplication.java
+++ src/main/java/com/bwl/apiusers/ApiUsersApplication.java
@@ -2,8 +2,11 @@ package com.bwl.apiusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+/* remove when the security config is made */
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class ApiUsersApplication {
 
 	public static void main(String[] args) {
