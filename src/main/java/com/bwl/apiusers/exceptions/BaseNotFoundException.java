ee351e67daa0542b686ecba7db24aa44e55d4c26
Walter Bernal <wltrbm@gmail.com>
2023-11-16 12:57:06 -0700

    Add of constructor

--- src/main/java/com/bwl/apiusers/exceptions/BaseNotFoundException.java
+++ src/main/java/com/bwl/apiusers/exceptions/BaseNotFoundException.java
@@ -1,5 +1,7 @@
 package com.bwl.apiusers.exceptions;
 
+import com.fasterxml.jackson.databind.ser.Serializers;
+
 public class BaseNotFoundException extends RuntimeException {
     public BaseNotFoundException(Class<?> entityType, Integer id) {
         super("Could not find " + entityType.getSimpleName() + " with id: " + id);
@@ -7,5 +9,9 @@ public class BaseNotFoundException extends RuntimeException {
 
     public BaseNotFoundException(Class<?> entityType) {
         super("Could not find any " + entityType.getSimpleName() + "s that could match such fields");
+    }
+
+    public BaseNotFoundException(Class<?> entityType, String message) {
+        super("Error in " + entityType.getSimpleName() + ": " + message);
     }
 }
