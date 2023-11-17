0a1e613577e00ad54645aab729955058b96309a1
Walter Bernal <wltrbm@gmail.com>
2023-11-17 15:44:49 -0700

    UserApplication and UserRepository added method to findAllByIdUser(User user)

--- src/main/java/com/bwl/apiusers/repositories/UserApplicationRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/UserApplicationRepository.java
@@ -1,9 +1,13 @@
 package com.bwl.apiusers.repositories;
 
+import com.bwl.apiusers.models.User;
 import com.bwl.apiusers.models.UserApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserApplicationRepository extends JpaRepository<UserApplication, Integer> {
+    List<UserApplication> findAllByIdUser(User user);
 }
--- src/main/java/com/bwl/apiusers/repositories/UserProfileRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/UserProfileRepository.java
@@ -1,9 +1,13 @@
 package com.bwl.apiusers.repositories;
 
+import com.bwl.apiusers.models.User;
 import com.bwl.apiusers.models.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Integer> {
+    List<UserProfile> findAllByIdUser(User user);
 }
