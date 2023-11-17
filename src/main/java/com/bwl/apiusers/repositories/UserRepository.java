623b6a06284e3cd4d3a9b8ef95fabe18284966bb
Walter Bernal <wltrbm@gmail.com>
2023-11-17 08:08:20 -0700

    Add of findAllByUserName method in UserReposotory

--- src/main/java/com/bwl/apiusers/repositories/UserRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/UserRepository.java
@@ -4,9 +4,12 @@ import com.bwl.apiusers.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends BaseRepository<User> {
     Optional<User> findOneByEmail(String email);
+    Optional<User> findOneByUserName(String userName);
+    List<User> findAllByUserName(String userName);
 }
