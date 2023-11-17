d212e2acf718530cbfd97af16aaa9a28a18de9b2
Walter Bernal <wltrbm@gmail.com>
2023-11-17 09:56:31 -0700

    Profile service implementation

--- src/main/java/com/bwl/apiusers/controllers/ProfileController.java
+++ src/main/java/com/bwl/apiusers/controllers/ProfileController.java
@@ -1,36 +1,25 @@
 package com.bwl.apiusers.controllers;
 
-import com.bwl.apiusers.assemblers.ProfileModelAssembler;
-import com.bwl.apiusers.exceptions.BaseNotFoundException;
-import com.bwl.apiusers.exceptions.ErrorResponse;
-import com.bwl.apiusers.models.Profile;
-import com.bwl.apiusers.dtos.ProfileDTO;
-import com.bwl.apiusers.repositories.ProfileRepository;
-import com.bwl.apiusers.utils.Utils;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.http.HttpStatus;
+import com.bwl.apiusers.services.ProfileService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/profiles")
-public class ProfileController extends GenericReadController<Profile, ProfileDTO, ProfileRepository, ProfileModelAssembler> {
-    ProfileController(ProfileRepository repository, ProfileModelAssembler assembler) {
-        super(repository, assembler, Profile.class, ProfileDTO.class);
+public class ProfileController implements GenericController {
+    private final ProfileService service;
+
+    public ProfileController(ProfileService service) {
+        this.service = service;
     }
 
-    @GetMapping("/{id}")
     @Override
-    public ResponseEntity<?> one(@PathVariable Integer id) {
-        try {
-            Profile profile = getRepository().findById(id).orElseThrow(() -> new BaseNotFoundException(Profile.class, id));
-            ProfileDTO dto = Utils.convertToDTO(profile, ProfileDTO.class);
-            EntityModel<ProfileDTO> entityModel = getAssembler().toModel(dto);
-            return ResponseEntity.ok(entityModel);
-        } catch (Exception e) {
-            ErrorResponse errorresponse = new ErrorResponse(e.getMessage());
-            return new ResponseEntity<>(errorresponse, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<?> one(Integer id) {
+        return service.one(id);
     }
 
+    @Override
+    public ResponseEntity<?> all(String name, int page, int size, String[] sort) {
+        return service.all(name, page, size, sort);
+    }
 }
---
+++ src/main/java/com/bwl/apiusers/services/ProfileService.java
@@ -0,0 +1,35 @@
+package com.bwl.apiusers.services;
+
+import com.bwl.apiusers.assemblers.ProfileModelAssembler;
+import com.bwl.apiusers.dtos.DTO;
+import com.bwl.apiusers.dtos.ProfileDTO;
+import com.bwl.apiusers.exceptions.BaseNotFoundException;
+import com.bwl.apiusers.exceptions.ErrorResponse;
+import com.bwl.apiusers.models.Profile;
+import com.bwl.apiusers.repositories.ProfileRepository;
+import com.bwl.apiusers.utils.Utils;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Service
+public class ProfileService extends GenericReadService<Profile, DTO, ProfileRepository, ProfileModelAssembler> {
+    public ProfileService(ProfileRepository repository, ProfileModelAssembler assembler) {
+        super(repository, assembler, Profile.class, DTO.class);
+    }
+
+    @Override
+    public ResponseEntity<?> one(@PathVariable Integer id) {
+        try {
+            Profile profile = getRepository().findById(id).orElseThrow(() -> new BaseNotFoundException(Profile.class, id));
+            ProfileDTO dto = Utils.convertToDTO(profile, ProfileDTO.class);
+            EntityModel<ProfileDTO> entityModel = getAssembler().toModel(dto);
+            return ResponseEntity.ok(entityModel);
+        } catch (Exception e) {
+            ErrorResponse errorresponse = new ErrorResponse(e.getMessage());
+            return new ResponseEntity<>(errorresponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
