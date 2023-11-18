159b5d21b97ae9de3d90e2e1a3f5c9117b9a1014
Walter Bernal <wltrbm@gmail.com>
2023-11-17 17:57:28 -0700

    UserService implementation in UserController

--- src/main/java/com/bwl/apiusers/controllers/UserController.java
+++ src/main/java/com/bwl/apiusers/controllers/UserController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/users")
-public class UserController1 implements GenericCRUDController<NewUserDTO, UpdateUserDTO> {
+public class UserController implements GenericCRUDController<NewUserDTO, UpdateUserDTO> {
     private final UserService service;
 
-    public UserController1(UserService service) {
+    public UserController(UserService service) {
         this.service = service;
     }
 
