1ebe2d6ef43eb680a430e011b3b6776cd9c1342b
Walter Bernal <wltrbm@gmail.com>
2023-11-21 08:21:47 -0700

    Creation of DTO for NewProject

---
+++ src/main/java/com/bwl/apiusers/dtos/NewProjectDTO.java
@@ -0,0 +1,13 @@
+package com.bwl.apiusers.dtos;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class NewProjectDTO implements DTO {
+    private String name;
+    private String keycode;
+    private String description;
+    private Integer idClient;
+}
