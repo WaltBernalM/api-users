561f8ccf42c76f4d17470ceab3b9f6c8632befce
Walter Bernal <wltrbm@gmail.com>
2023-11-21 16:47:46 -0700

    Creation of updateProject method

--- src/main/java/com/bwl/apiusers/services/ProjectService.java
+++ src/main/java/com/bwl/apiusers/services/ProjectService.java
@@ -3,18 +3,15 @@ package com.bwl.apiusers.services;
 import com.bwl.apiusers.assemblers.ProjectModelAssembler;
 import com.bwl.apiusers.dtos.NewProjectDTO;
 import com.bwl.apiusers.dtos.ProjectDTO;
+import com.bwl.apiusers.dtos.UpdateProjectDTO;
 import com.bwl.apiusers.exceptions.BaseNotFoundException;
 import com.bwl.apiusers.exceptions.ErrorResponse;
-import com.bwl.apiusers.models.Application;
-import com.bwl.apiusers.models.ApplicationProject;
-import com.bwl.apiusers.models.Client;
-import com.bwl.apiusers.models.Project;
+import com.bwl.apiusers.models.*;
 import com.bwl.apiusers.repositories.ApplicationProjectRepository;
 import com.bwl.apiusers.repositories.ApplicationRepository;
 import com.bwl.apiusers.repositories.ClientRepository;
 import com.bwl.apiusers.repositories.ProjectRepository;
 import com.bwl.apiusers.utils.Utils;
-import jdk.jshell.execution.Util;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,10 +19,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.lang.reflect.Field;
+import java.util.*;
 
 @Service
 public class ProjectService extends GenericReadService<Project, ProjectDTO, ProjectRepository, ProjectModelAssembler> {
@@ -80,7 +75,7 @@ public class ProjectService extends GenericReadService<Project, ProjectDTO, Proj
 
         } catch (Exception e) {
             ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
-            return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -93,7 +88,80 @@ public class ProjectService extends GenericReadService<Project, ProjectDTO, Proj
             return ResponseEntity.ok(entityModel);
         } catch( Exception e) {
             ErrorResponse errorresponse = new ErrorResponse(e.getMessage());
-            return new ResponseEntity<>(errorresponse, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(errorresponse, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity<?> updateProject(@PathVariable Integer id, @RequestBody UpdateProjectDTO updateProjectDTO) {
+        try {
+            Project projectInDb = projectRepository.findById(id)
+                    .orElseThrow(() -> new BaseNotFoundException(Project.class, "id not found in database"));
+
+            // Verify if Keycode already taken
+            String updateDTOKeycode = updateProjectDTO.getKeycode();
+            List<Project> projectsInDb = projectRepository.findAllByKeycode(updateDTOKeycode);
+            if (!projectsInDb.isEmpty()) {
+                Project first = projectsInDb.getFirst();
+                if (!Objects.equals(first.getId(), id)) {
+                    throw new BaseNotFoundException(Project.class, "keycode already taken by another Project");
+                }
+            }
+
+            // Verify if idClient do exists.
+            Integer clientId = updateProjectDTO.getIdClient();
+            Optional<Client> clientInDb = Optional.empty();
+            if (clientId != null) {
+                clientInDb = Utils.verifyExistence(clientId, clientRepository, Client.class);
+            }
+
+            // Verify if idApplication do exists.
+            Integer applicationId = updateProjectDTO.getIdApplication();
+            Optional<Application> applicationInDb = Optional.empty();
+            if (applicationId != null) {
+                applicationInDb = Utils.verifyExistence(applicationId, applicationRepository, Application.class);
+            }
+
+            // Find all projects with idProject and save if it doesn't exist
+            List<ApplicationProject> allApplicationsRelatedToProject = applicationProjectRepository.findAllByIdProject(projectInDb);
+
+            // Block to save the ApplicationProject if is not duplicated
+            boolean isDuplicated = false;
+            for (ApplicationProject row: allApplicationsRelatedToProject) {
+                Project project = row.getIdProject();
+                Application application = row.getIdApplication();
+                if (project.equals(projectInDb) && applicationInDb.isPresent() && application.equals(applicationInDb.get())) {
+                    isDuplicated = true;
+                }
+            }
+            if (!isDuplicated && applicationInDb.isPresent()) {
+                addNewApplicationProjectRow(projectInDb, applicationInDb.get());
+            }
+
+            Map<String, Object> body = new HashMap<>();
+
+            // Update of projectInDB values as of updateProjectDTO
+            for(Field field : updateProjectDTO.getClass().getDeclaredFields()) {
+                field.setAccessible(true);
+                Object value = field.get(updateProjectDTO);
+                if (value != null && !field.getName().equals("idClient")) {
+                    Field projectField = Project.class.getDeclaredField(field.getName());
+                    projectField.setAccessible(true);
+                    projectField.set(projectInDb, value);
+                }
+            }
+            clientInDb.ifPresent(projectInDb::setIdClient);
+
+            Project updatedProject = projectRepository.save(projectInDb);
+            ProjectDTO updatedProjectData = Utils.convertToDTO(updatedProject, ProjectDTO.class);
+
+            body.put("project", updatedProjectData);
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("data", body);
+            return new ResponseEntity<>(response, HttpStatus.OK) ;
+        } catch (Exception e) {
+            ErrorResponse errorresponse = new ErrorResponse(e.getMessage());
+            return new ResponseEntity<>(errorresponse, HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -105,7 +173,6 @@ public class ProjectService extends GenericReadService<Project, ProjectDTO, Proj
         newProject.setIdClient(idClient);
         newProject.setCreationDate(new Date());
         newProject.setEnabled(true);
-
         return newProject;
     }
 
