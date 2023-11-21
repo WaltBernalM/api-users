fea742306ebab18944bb9faf9dfcacc31dfac15a
Walter Bernal <wltrbm@gmail.com>
2023-11-21 10:15:52 -0700

    Implementation of Project controller

---
+++ src/main/java/com/bwl/apiusers/controllers/ProjectController.java
@@ -0,0 +1,44 @@
+package com.bwl.apiusers.controllers;
+
+
+import com.bwl.apiusers.dtos.NewProjectDTO;
+import com.bwl.apiusers.dtos.UpdateProjectDTO;
+import com.bwl.apiusers.services.ProjectService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController implements GenericCRUDController<NewProjectDTO, UpdateProjectDTO>{
+    private final ProjectService service;
+
+    public ProjectController(ProjectService service) {
+        this.service = service;
+    }
+
+    @Override
+    public ResponseEntity<?> postNew(NewProjectDTO newProjectDTO) {
+        return service.newProjectSignup(newProjectDTO);
+    }
+
+    @Override
+    public ResponseEntity<?> updateUser(Integer id, UpdateProjectDTO updateEntityDTO) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> deleteUser(Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> one(Integer id) {
+        return service.one(id);
+    }
+
+    @Override
+    public ResponseEntity<?> all(String name, int page, int size, String[] sort) {
+        return service.all(name, page, size, sort);
+    }
+}
