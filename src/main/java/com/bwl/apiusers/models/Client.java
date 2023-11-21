b6ce69835015e6a01e0b35b13128b673b568ee93
Walter Bernal <wltrbm@gmail.com>
2023-11-21 10:16:58 -0700

    Creation of Client model

---
+++ src/main/java/com/bwl/apiusers/models/Client.java
@@ -0,0 +1,23 @@
+package com.bwl.apiusers.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "CLIENTS")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "SHORT_NAME", nullable = false)
+    private String shortName;
+
+    @Column(name = "ENABLED", nullable = false)
+    private Boolean enabled;
+}
