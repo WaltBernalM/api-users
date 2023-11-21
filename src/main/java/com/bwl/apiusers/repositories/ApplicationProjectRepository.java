9e683ff3866d16eff4eb968567de45443d2e802f
Walter Bernal <wltrbm@gmail.com>
2023-11-21 10:17:15 -0700

    Creation of ApplicationProject repository

---
+++ src/main/java/com/bwl/apiusers/repositories/ApplicationProjectRepository.java
@@ -0,0 +1,7 @@
+package com.bwl.apiusers.repositories;
+
+import com.bwl.apiusers.models.ApplicationProject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationProjectRepository extends JpaRepository<ApplicationProject, Integer> {
+}
