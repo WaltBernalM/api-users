b8c581913d821a6a299dbe9f91c039690b6ad087
Walter Bernal <wltrbm@gmail.com>
2023-11-21 12:18:31 -0700

    Project repository addition of method to find all by idClient

--- src/main/java/com/bwl/apiusers/repositories/ProjectRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/ProjectRepository.java
@@ -1,5 +1,6 @@
 package com.bwl.apiusers.repositories;
 
+import com.bwl.apiusers.models.Client;
 import com.bwl.apiusers.models.Project;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.Optional;
 public interface ProjectRepository extends  BaseRepository<Project> {
     Optional<Project> findOneByKeycode(String keycode);
     List<Project> findAllByKeycode(String keycode);
+    List<Project> findAllByIdClient(Client idClient);
 }
