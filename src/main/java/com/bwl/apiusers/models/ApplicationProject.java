915f3113f9194a464792ab1b8ca35a661f3b0854
Walter Bernal <wltrbm@gmail.com>
2023-11-21 10:16:43 -0700

    Creation of ApplicationProject model

---
+++ src/main/java/com/bwl/apiusers/models/ApplicationProject.java
@@ -0,0 +1,21 @@
+package com.bwl.apiusers.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "APPLICATIONS_PROJECTS")
+public class ApplicationProject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_APPLICATION", nullable = false)
+    private Application idApplication;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_PROJECT", nullable = false)
+    private Project idProject;
+}
