c3662629f5a88213457efa2bfbb3c457f30b7217
Walter Bernal <wltrbm@gmail.com>
2023-11-21 16:48:43 -0700

    Addition of findAllByIdProject method

--- src/main/java/com/bwl/apiusers/repositories/ApplicationProjectRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/ApplicationProjectRepository.java
@@ -1,7 +1,13 @@
 package com.bwl.apiusers.repositories;
 
+import com.bwl.apiusers.models.Application;
 import com.bwl.apiusers.models.ApplicationProject;
+import com.bwl.apiusers.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApplicationProjectRepository extends JpaRepository<ApplicationProject, Integer> {
+    List<ApplicationProject> findAllByIdProject(Project idProject);
+    List<ApplicationProject> findAllByIdApplication(Application idApplication);
 }
