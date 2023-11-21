d153f112f7110b70494e4af73a5af931a15e480c
Walter Bernal <wltrbm@gmail.com>
2023-11-21 12:15:08 -0700

    Creation of findOne and findAll by name and by shortname

--- src/main/java/com/bwl/apiusers/repositories/ClientRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/ClientRepository.java
@@ -2,5 +2,12 @@ package com.bwl.apiusers.repositories;
 
 import com.bwl.apiusers.models.Client;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ClientRepository extends BaseRepository<Client>{
+    Optional<Client> findOneByName(String name);
+    Optional<Client> findOneByShortName(String shortName);
+    List<Client> findAllByName(String name);
+    List<Client> findAllByShortName(String shortName);
 }
