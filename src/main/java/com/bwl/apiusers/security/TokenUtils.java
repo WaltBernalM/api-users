4ee0ebd4b86328d0d04ce5b107a8a96a9a513ef8
Walter Bernal <wltrbm@gmail.com>
2023-11-22 14:07:13 -0700

    Addition of authorities inside token

--- src/main/java/com/bwl/apiusers/security/TokenUtils.java
+++ src/main/java/com/bwl/apiusers/security/TokenUtils.java
@@ -6,20 +6,28 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import javax.crypto.SecretKey;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class TokenUtils {
     private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     private final static Long ACCESS_TOKEN_VALIDITY_SECONDS = 2_000L;
 
-    public static String createToken(Integer idApplication, String username) {
+    public static String createToken(
+            Integer idApplication,
+            String username,
+            Collection<? extends GrantedAuthority> authorities) {
+
         long expirationTime = ACCESS_TOKEN_VALIDITY_SECONDS * 1_000;
         Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
 
         Map<String, Object> extra = new HashMap<>();
         extra.put("idApplication", idApplication);
+        extra.put("authorities", authorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
 
         return Jwts.builder()
                 .setSubject(username)
@@ -38,9 +46,27 @@ public class TokenUtils {
                     .getBody();
 
             String username = claims.getSubject();
-            return new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+            Collection<? extends GrantedAuthority> authorities = getAuthoritiesFromClaims(claims);
+            System.out.println("authoritiesInToken: " + authorities);
+
+            return new UsernamePasswordAuthenticationToken(username, null, authorities);
         } catch (JwtException e) {
             return null;
         }
     }
+
+    private  static Collection<? extends GrantedAuthority> getAuthoritiesFromClaims(Claims claims) {
+        Object authoritiesClaim = claims.get("authorities");
+
+        if (authoritiesClaim instanceof List) {
+            List<?> authoritiesList = (List<?>) authoritiesClaim;
+            return authoritiesList.stream()
+                    .map(Object::toString)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        }
+
+        return Collections.emptyList();
+    }
 }
+
