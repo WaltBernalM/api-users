ce40b511d86a37b4cfd471d98791f1b224063e5b
Walter Bernal <wltrbm@gmail.com>
2023-11-22 14:22:41 -0700

    implementation of preAuthorize for all Reading to be ROLE_USER

--- src/main/java/com/bwl/apiusers/controllers/GenericReadController.java
+++ src/main/java/com/bwl/apiusers/controllers/GenericReadController.java
@@ -1,6 +1,7 @@
 package com.bwl.apiusers.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface GenericReadController {
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<?> one(@PathVariable Integer id);
 
     @GetMapping("")
