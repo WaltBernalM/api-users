c154eaf420894c1708c85831134a9c677f59fa18
Walter Bernal <wltrbm@gmail.com>
2023-11-22 00:30:44 -0700

    Removal of unused imports

--- src/main/java/com/bwl/apiusers/repositories/UserProfileRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/UserProfileRepository.java
@@ -3,11 +3,6 @@ package com.bwl.apiusers.repositories;
 import com.bwl.apiusers.models.UserProfile;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-//public interface UserProfileRepository extends JpaRepository<UserProfile, Integer> {
-//    List<UserProfile> findAllByIdUser(User user);
-//}
 public interface UserProfileRepository extends UserComposedRepository<UserProfile> {
 }
