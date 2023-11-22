8eea519fc326e4bbaf4b358246292a45bb76f40a
Walter Bernal <wltrbm@gmail.com>
2023-11-22 00:30:07 -0700

    Implementation of findAllByIdApplication method

--- src/main/java/com/bwl/apiusers/repositories/ProfileRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/ProfileRepository.java
@@ -1,6 +1,10 @@
 package com.bwl.apiusers.repositories;
 
+import com.bwl.apiusers.models.Application;
 import com.bwl.apiusers.models.Profile;
 
+import java.util.List;
+
 public interface ProfileRepository extends BaseRepository<Profile> {
+    List<Profile> findAllByIdApplication (Application idApplication);
 }
