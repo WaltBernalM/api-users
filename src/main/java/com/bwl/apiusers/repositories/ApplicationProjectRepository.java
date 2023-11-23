309298026c62a91bbe4f7212ec8c0574e8d125a3
Walter Bernal <wltrbm@gmail.com>
2023-11-23 13:19:08 -0700

    Addition of method to retrieve one by idProject in ApplicationProject repository

--- src/main/java/com/bwl/apiusers/repositories/ApplicationProjectRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/ApplicationProjectRepository.java
@@ -6,8 +6,10 @@ import com.bwl.apiusers.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ApplicationProjectRepository extends JpaRepository<ApplicationProject, Integer> {
     List<ApplicationProject> findAllByIdProject(Project idProject);
     List<ApplicationProject> findAllByIdApplication(Application idApplication);
+    Optional<ApplicationProject> findOneByIdProject(Project idProject);
 }
