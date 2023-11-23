f7ad984f09abe075810ecef201a7b91d036db0b5
Walter Bernal <wltrbm@gmail.com>
2023-11-23 15:42:46 -0700

    Addition of findOneByIdUser in UserComposedRepository interface

--- src/main/java/com/bwl/apiusers/repositories/UserComposedRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/UserComposedRepository.java
@@ -1,6 +1,7 @@
 package com.bwl.apiusers.repositories;
 
 import com.bwl.apiusers.models.User;
+import com.bwl.apiusers.models.UserApplication;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -8,8 +9,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.List;
+import java.util.Optional;
 
 @NoRepositoryBean
 public interface UserComposedRepository<T> extends JpaRepository<T, Integer> {
     List<T> findAllByIdUser(User idUser);
+    Optional<UserApplication> findOneByIdUser(User user);
 }
