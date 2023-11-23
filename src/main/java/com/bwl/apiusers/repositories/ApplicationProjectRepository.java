e07c48a2a6dcc18136bdd498712d53634d116ac4
Walter Bernal <wltrbm@gmail.com>
2023-11-23 15:43:17 -0700

    Deletion of unused method in ApplicaitonProjectRepository

--- src/main/java/com/bwl/apiusers/repositories/ApplicationProjectRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/ApplicationProjectRepository.java
@@ -1,6 +1,5 @@
 package com.bwl.apiusers.repositories;
 
-import com.bwl.apiusers.models.Application;
 import com.bwl.apiusers.models.ApplicationProject;
 import com.bwl.apiusers.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +9,5 @@ import java.util.Optional;
 
 public interface ApplicationProjectRepository extends JpaRepository<ApplicationProject, Integer> {
     List<ApplicationProject> findAllByIdProject(Project idProject);
-    List<ApplicationProject> findAllByIdApplication(Application idApplication);
     Optional<ApplicationProject> findOneByIdProject(Project idProject);
 }
