1c0ed99e1655de1feb5233fccdef9664c7e1ffc8
Walter Bernal <wltrbm@gmail.com>
2023-11-22 17:10:21 -0700

    removal of unused imports

--- src/main/java/com/bwl/apiusers/dtos/UserDTO.java
+++ src/main/java/com/bwl/apiusers/dtos/UserDTO.java
@@ -3,8 +3,6 @@ package com.bwl.apiusers.dtos;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-
 @Data
 @Component
 public class UserDTO implements DTO {
