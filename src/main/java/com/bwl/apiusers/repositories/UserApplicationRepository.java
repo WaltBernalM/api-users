30bdfcdbbd8361f441f4dc86e039db338901965c
Walter Bernal <wltrbm@gmail.com>
2023-11-23 12:42:47 -0700

    Addition of method in UserApplication repository to find one by idUser

--- src/main/java/com/bwl/apiusers/repositories/UserApplicationRepository.java
+++ src/main/java/com/bwl/apiusers/repositories/UserApplicationRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface UserApplicationRepository extends UserComposedRepository<UserApplication>{
     Optional<UserApplication> findOneByIdUserAndIdApplication(User user, Application application);
+    Optional<UserApplication> findOneByIdUser(User user);
 }
