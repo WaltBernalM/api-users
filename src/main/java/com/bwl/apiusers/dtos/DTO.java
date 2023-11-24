2a05c04b4aef48421bf860e1c1c68a7fefde5e00
Walter Bernal <wltrbm@gmail.com>
2023-11-24 16:54:06 -0700

    Addition of default methods in DTO interface

--- src/main/java/com/bwl/apiusers/dtos/DTO.java
+++ src/main/java/com/bwl/apiusers/dtos/DTO.java
@@ -1,4 +1,7 @@
 package com.bwl.apiusers.dtos;
 
 public interface DTO {
+    default String getKeycode() {return null;}
+    default String getShortName() {return null;}
+    default String getUsername() {return null;}
 }
