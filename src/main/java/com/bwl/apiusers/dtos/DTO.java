4e02c73019fcb701c968a3fdc6fbb5e2ac51441c
Walter Bernal <wltrbm@gmail.com>
2023-11-24 18:03:16 -0700

    removal of methos in DTO

--- src/main/java/com/bwl/apiusers/dtos/DTO.java
+++ src/main/java/com/bwl/apiusers/dtos/DTO.java
@@ -1,7 +1,4 @@
 package com.bwl.apiusers.dtos;
 
 public interface DTO {
-    default String getKeycode() {return null;}
-    default String getShortName() {return null;}
-    default String getUsername() {return null;}
 }
